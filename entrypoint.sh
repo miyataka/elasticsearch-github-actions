789223dd1d6c5c472242b3d4cb96765a60a7c8ea
Takayuki Miyahara <voyager.3taka28@gmail.com>
2020-04-23 22:52:41 +0900

    fix: multiple plugin install

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,16 @@ docker network create elastic
 PLUGINS_STR=`echo ${PLUGINS} | sed -e 's/\n/ /g'`
 MAJOR_VERSION=`echo ${STACK_VERSION} | cut -c 1`
 
+PLUGIN_INSTALL_CMD=""
+
+if [ "x${PLUGINS_STR}" != "x" ]; then
+    ARRAY=(${PLUGINS_STR})
+    for i in ${ARRAY}
+    do
+        PLUGIN_INSTALL_CMD+="elasticsearch-plugin install --batch ${i} &&"
+    done
+fi
+
 # single node only
 if [ "x${MAJOR_VERSION}" == 'x6' ]; then
   docker run \
@@ -31,7 +41,7 @@ if [ "x${MAJOR_VERSION}" == 'x6' ]; then
     --name="es1" \
     --entrypoint="" \
     docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION} \
-    /bin/sh -vc "elasticsearch-plugin install --batch ${PLUGINS_STR} && /usr/local/bin/docker-entrypoint.sh"
+    /bin/sh -vc "${PLUGIN_INSTALL_CMD} /usr/local/bin/docker-entrypoint.sh"
 elif [ "x${MAJOR_VERSION}" == 'x7' ]; then
   docker run \
     --rm \
@@ -52,7 +62,7 @@ elif [ "x${MAJOR_VERSION}" == 'x7' ]; then
     --name="es1" \
     --entrypoint="" \
     docker.elastic.co/elasticsearch/elasticsearch:${STACK_VERSION} \
-    /bin/sh -vc "elasticsearch-plugin install --batch ${PLUGINS_STR} && /usr/local/bin/docker-entrypoint.sh"
+    /bin/sh -vc "${PLUGIN_INSTALL_CMD} /usr/local/bin/docker-entrypoint.sh"
 fi
 
 docker run \
